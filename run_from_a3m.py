0d4a5a0c0186613939117f3e4a96befda47c2599
PadX <40377608+Paddy-Xu@users.noreply.github.com>
2026-03-04 11:12:18 +0100

    sb

---
+++ run_from_a3m.py
@@ -0,0 +1,58 @@
+from run_no_docker import *
+import sys
+
+import run_alphafold as run_alphafold
+from pipeline_pre_run import *
+
+from alphafold.data import pipeline
+from alphafold.data import pipeline_multimer
+
+DataPipelineNew.process = DataPipelineNew.process_a3m
+
+pipeline.DataPipeline = DataPipelineNew
+pipeline_multimer.DataPipeline = DataPipelineMultimerNew
+
+if __name__ == '__main__':
+
+    flags.mark_flags_as_required([
+        'data_dir',
+        'fasta_paths',
+        'max_template_date',
+    ])
+
+    fasta_paths = 'O15552.fasta'
+
+
+    output_dir = "/scratch/project_465002572/af3_self/runs/precompute_msa/"
+
+    db_dir = Path('/scratch/project_465002572/uniprot_test/deep_mind_dataset').absolute()
+
+    model_dir = '../alphafold_models'
+
+    new_argv = sys.argv[:]
+
+    if not any(a.startswith("--fasta_paths=") for a in new_argv):
+        new_argv.append(f"--fasta_paths={fasta_paths}")
+
+    if not any(a.startswith("--max_template_date=") for a in new_argv):
+        new_argv.append(f"--max_template_date=2020-05-14")
+
+    if not any(a.startswith("--model_preset=") for a in new_argv):
+        new_argv.append(f"--model_preset=monomer")
+
+    if not any(a.startswith("--db_preset=") for a in new_argv):
+        new_argv.append(f"--db_preset=reduced_dbs")
+    if not any(a.startswith("--data_dir=") for a in new_argv):
+        new_argv.append(f"--data_dir={db_dir}")
+    if not any(a.startswith("--use_precomputed_msas=") for a in new_argv):
+        new_argv.append(f"--use_precomputed_msas=True")
+
+    if not any(a.startswith("--output_dir=") for a in new_argv):
+        new_argv.append(f"--output_dir={output_dir}")
+
+    sys.argv = new_argv
+
+
+    app.run(main)
+
+
